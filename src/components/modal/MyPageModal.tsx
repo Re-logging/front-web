9cecba665fc718484435a4bf5e2fbbbcb5321f06
Devicii <62472550+asroq1@users.noreply.github.com>
2024-11-13 23:41:58 +0900

    FEAT: 마이페이지 관련 컴포넌트 추가

--- src/components/modal/MyPageModal.tsx
+++ src/components/modal/MyPageModal.tsx
@@ -12,9 +12,9 @@ export function MyPageModal() {
     mode: 'mypage',
   })
 
-  const handleProfileClick = () => {
+  const handleLocationClick = (location: string) => {
     closeModal()
-    router.push('/profile')
+    router.push(`/${location}`)
   }
 
   return (
@@ -28,13 +28,13 @@ export function MyPageModal() {
       className="h-full max-h-[280px] w-full max-w-[368px] bg-white"
     >
       <Button
-        onClick={handleProfileClick}
+        onClick={() => handleLocationClick('profile')}
         className="rounded-md bg-white px-3 py-2 text-sm font-medium text-text"
       >
         프로필 관리 및 수정
       </Button>
       <Button
-        onClick={handleProfileClick}
+        onClick={() => handleLocationClick('account')}
         className="rounded-md bg-white px-3 py-2 text-sm font-medium text-text"
       >
         계정 관리 및 수정
